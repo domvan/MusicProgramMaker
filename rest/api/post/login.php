2780bee0692811e9630053e42b19b300660daaca
domvan <79351650+domvan@users.noreply.github.com>
2021-11-24 23:32:47 -0700

    added login.php

---
+++ rest/api/post/login.php
@@ -0,0 +1,29 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>loginpage</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <form action="">
+    <div class="container">
+      <div class="login-box">
+        <div>
+          <label for="username"><b>Username:</b></label>
+          <input type="username" placeholder="johndoe123" required/>
+        </div>
+        <div>
+          <label for="Password"><b>Password:</b></label>
+          <input type="password" placeholder="********" required />
+        </div>
+        <input type="submit" value="Login" class="submitbutton" />
+        <div class="bottom">
+          <p><a href="ForgotPW.html">Forgot password</a></p>
+          <p><a href="Register.html">Register now</a></p>
+        </div>
+      </div>
+    </div>
+  </form>
+  </body>
+</html>
