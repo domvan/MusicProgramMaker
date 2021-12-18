a5f066bec6aaf88341508bbcfbcbe23511aa5b1e
bretshilliday <47432093+bretshilliday@users.noreply.github.com>
2021-12-18 10:15:15 -0700

    API
    This is the api

--- rest/models/Performer.php
+++ rest/models/Performer.php
@@ -1,6 +1,5 @@
 <?php
     class Performer {
-        // DB stuff
         private $conn;
         private $table = 'performer';
 
@@ -29,6 +28,42 @@
             return $stmt;
 
         }
+        public function create() {
+            // Create Query
+            $query = 'INSERT INTO ' .
+            $this->table . '
+            SET
+            name = :name,
+            age = :age,
+            bio = :bio,
+            classID = :classID,
+            levelID = :levelID';
 
-        //Test comment
+        // Prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Clean data
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->age = htmlspecialchars(strip_tags($this->age));
+        $this->bio = htmlspecialchars(strip_tags($this->bio));
+        $this->classID = htmlspecialchars(strip_tags($this->classID));
+        $this->levelID = htmlspecialchars(strip_tags($this->levelID));
+
+        // Bind data
+        $stmt-> bindParam(':name', $this->name);
+        $stmt-> bindParam(':age', $this->age);
+        $stmt-> bindParam(':bio', $this->bio);
+        $stmt-> bindParam(':classID', $this->classID);
+        $stmt-> bindParam(':levelID', $this->levelID);
+
+
+        // Execute query
+        if($stmt->execute()) {
+            return true;
+        }
+        // Print error if something goes wrong
+        printf("Error: $s.\n", $stmt->error);
+
+        return false;
+        }
     }
