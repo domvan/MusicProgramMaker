dd7843399c96ab09a73560bdce462e676d66f85a
bretshilliday <47432093+bretshilliday@users.noreply.github.com>
2021-12-18 15:37:18 -0700

    Final Push
    Removed bad comments and cleaned up for submission

--- ManageClass.php
+++ ManageClass.php
@@ -16,13 +16,11 @@
    <body>
    <a href="ManageClasses.php">&#8592 Back</a>
       <center>
-         <!-- Populates with an image at the top and will take you back to the default view (not searched) if clicked -->
          <div class ="taskbar">
             <a href="/MusicProgramMaker/ManageClasses.php">
             <img src="https://pngimg.com/uploads/music_notes/music_notes_PNG38.png" width="600" height="200"> 
             </a>
          </div>
-         <!-- Add New Class Form -->
          <?php
          $id = str_replace("+"," ",$_GET['id']);
          echo "<h2 class='header'> Add Student to ".$id."</h2>";
@@ -31,37 +29,29 @@
             <label for="text">
                Student:
                <select name="sName">
-                  <!-- Gets all composers from the database and then adds them to the drop down of selectable values -->
                   <?php 
                      $sql = mysqli_query($connection, "SELECT name FROM performer WHERE classID IS NULL");
                      while ($row = $sql->fetch_assoc()){
                        echo '<option value="'.$row['name'].'"> '.$row['name'].' </option>';
                      }
                      ?>
-                  <!-- Finish getting the composers -->
                </select>
             </label>
             <input type = "submit" name = "insertClass" value = "Add"> 
          </form>
-         <!-- Finish Add New Class Form-->
          <br>
          </br>
-         <!-- Classs Data Table -->
          <div class ="tableview">
             <table width="1100" border="1" cellpadding="1" cellspacing="1">
                <tr>
-                 <!-- Classs Data Table Headers (Probably could get these from the database if necessary similar to the composer names) -->
                   <th>Name</th>
                   <th>Actions</th>
                </tr>
-               <?php
-                  // If the user clicks on the "Add" button then this query will be executed. NOTE: There is no field for data so I just added 2021 as a default but this could be left blank              
+               <?php            
                   $id = str_replace("+"," ",$_GET['id']);
                   if(isset($_POST["insertClass"])) {
                      mysqli_query($connection, "UPDATE performer SET classID = '$id' WHERE name = '$_POST[sName]'");
                   }
-                    
-               // If there was not a search and the page was just loaded fresh then all results should be shown
                   $query = "SELECT * FROM performer WHERE classID = '$id'";
                   $result = mysqli_query($connection, $query);
                   
--- ManageStudents.php
+++ ManageStudents.php
@@ -16,19 +16,16 @@
    <body>
    <a href="AdminLandingPage.php">&#8592 Back</a>
       <center>
-         <!-- Populates with an image at the top and will take you back to the default view (not searched) if clicked -->
          <div class ="taskbar">
             <a href="/MusicProgramMaker/ManageStudents.php">
             <img src="https://pngimg.com/uploads/music_notes/music_notes_PNG38.png" width="600" height="200"> 
             </a>
-            <!-- Search Form -->
             <h2> Search Students </h2>
             <form action ="ManageStudents.php" method="GET">
-               <input type="text" name="search" placeholder="Search for students"/> <!-- name is used to determine the query block below -->
+               <input type="text" name="search" placeholder="Search for students"/>
                <input type="submit" value="Search"/>
             </form>
          </div>
-         <!-- Add New Student Form -->
          <h2 class="header"> Add New Student </h2>
          <form action ="ManageStudents.php" method="POST">
             <label for="text">Name:</label>
@@ -41,27 +38,23 @@
                Class:
                <select name="className">
                <option value='NULL'>No Class</option>
-                  <!-- Gets all composers from the database and then adds them to the drop down of selectable values -->
                   <?php 
                      $sql = mysqli_query($connection, "SELECT classID FROM class");
                      while ($row = $sql->fetch_assoc()){
                        echo '<option value="'.$row['classID'].'"> '.$row['classID'].' </option>';
                      }
                      ?>
-                  <!-- Finish getting the composers -->
                </select>
             </label>
             <label for="text">
                Level:
                <select name="levelID">
-                  <!-- Gets all composers from the database and then adds them to the drop down of selectable values -->
                   <?php 
                      $sql = mysqli_query($connection, "SELECT levelID FROM level");
                      while ($row = $sql->fetch_assoc()){
                        echo '<option value="'.$row['levelID'].'"> '.$row['levelID'].' </option>';
                      }
                      ?>
-                  <!-- Finish getting the composers -->
                </select>
             </label>
             <label for="text">
@@ -75,14 +68,11 @@
             </label>
             <input type = "submit" name = "insertStudent" value = "Add"> 
          </form>
-         <!-- Finish Add New Student Form-->
          <br>
          </br>
-         <!-- Students Data Table -->
          <div class ="tableview">
             <table width="1100" border="1" cellpadding="1" cellspacing="1">
                <tr>
-                 <!-- Students Data Table Headers (Probably could get these from the database if necessary similar to the composer names) -->
                   <th>Name</th>
                   <th>Age</th>
                   <th>Bio</th>
@@ -90,8 +80,7 @@
                   <th>Level</th>
                   <th>Actions</th>
                </tr>
-               <?php
-                  // If the user clicks on the "Add" button then this query will be executed. NOTE: There is no field for data so I just added 2021 as a default but this could be left blank              
+               <?php          
                   if(isset($_POST["insertStudent"])) {
                      if($_POST["className"] = 'NULL'){
                         mysqli_query($connection, "INSERT into performer VALUES ('$_POST[name]', '$_POST[age]', '$_POST[bio]', NULL, '$_POST[levelID]')");
@@ -102,14 +91,12 @@
                      printf("Error: %s\n", $connection -> error);
                   }
                   
-                  // If the user clicks on the "search" button then this query will be executed and the page will be reloaded with the following query.
                   if(isset($_GET["search"])) {
                     $search = $_GET["search"];
                     $search = mysqli_real_escape_string($connection, $search);
                     
                     $query = "SELECT * FROM performer WHERE name LIKE '%$search%'";
                     
-                    // Assuming there was some result then we need to build the table
                     if(mysqli_query($connection, $query))
                     {
                     $result = mysqli_query($connection, $query);
@@ -133,7 +120,7 @@
                               
                       }
                     }
-                  }	else { // If there was not a search and the page was just loaded fresh then all results should be shown
+                  }	else {
                     $query = "SELECT * FROM performer";
                     $result = mysqli_query($connection, $query);
                     
